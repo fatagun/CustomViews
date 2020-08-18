866215216aa0d39e62a12974100574e8cefb7d5e
firat <fatagun@gmail.com>
2020-08-18 15:57:17 +0300

    first one

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using CustomViews.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+
+namespace CustomViews.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IHttpContextAccessor _httpcontextaccessor;
+        private readonly ISession _session;
+
+        public HomeController(ILogger<HomeController> logger, IHttpContextAccessor httpContextAccessor)
+        {
+            _logger = logger;
+            _httpcontextaccessor = httpContextAccessor;
+            _session = _httpcontextaccessor.HttpContext.Session;
+        }
+
+        public IActionResult Index()
+        {
+
+            var f = _httpcontextaccessor.HttpContext.Request.Headers["User-Agent"].ToString();
+            return View();
+        }
+
+        public IActionResult ToMobile()
+        {
+            _session.SetString("Device", "Mobile");
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult ToTablet()
+        {
+            _session.SetString("Device", "Tablet");
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult ToDesktop()
+        {
+            _session.SetString("Device", "Desktop");
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MobileViewLocationExpander.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Razor;
+
+namespace CustomViews
+{
+    public class MobileViewLocationExpander : IViewLocationExpander
+    {
+        public IEnumerable<string> ExpandViewLocations(ViewLocationExpanderContext context, IEnumerable<string> viewLocations)
+        {
+            
+            if(context.Values["Device"] == null)
+            {
+                return viewLocations;
+            }
+
+            if(context.Values["Device"] == "Mobile")
+            {
+                viewLocations = new[] {
+                    $"/Views/{{1}}/{{0}}.mobile.cshtml",
+                    $"/Views/Shared/{{0}}.mobile.cshtml",
+                }
+                .Concat(viewLocations);
+            }
+
+            if(context.Values["Device"] == "Tablet")
+            {
+                viewLocations = new[] {
+                    $"/Views/{{1}}/{{0}}.tablet.cshtml",
+                    $"/Views/Shared/{{0}}.tablet.cshtml",
+                }
+                .Concat(viewLocations);
+            }
+
+            return viewLocations;
+        }
+
+        public void PopulateValues(ViewLocationExpanderContext context)
+        {
+            var device = context.ActionContext.HttpContext.Session.GetString("Device");
+
+            context.Values["Device"] = device;
+        }
+    }
+}
